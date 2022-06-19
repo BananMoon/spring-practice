a900ebcb056fb6c96e8c8aea45b14ad7040e7754
banan99 <banan99@naver.com>
2022-06-19 23:22:22 +0900

    [FEAT] Member 클래스

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class Member {
+    @Id @GeneratedValue // entity의 primary key
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+    @Embedded   //내장타입 매핑
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // Order클래스의 필드 member가 연관관계 주인
+    private List<Order> orders = new ArrayList<>();
+
+    public Member(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+}

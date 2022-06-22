1d775834a37bb83d03c471d6834f235d2f68fb0c
banan99 <banan99@naver.com>
2022-06-22 15:53:08 +0900

    [FEAT] findAll() 추가: 동적쿼리 이용하여 검색

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderRepository {
     private final EntityManager em;
+//    new JPAQuery(em)
 
     public void save(Order order) {
         em.persist(order);
@@ -18,8 +19,33 @@ public class OrderRepository {
     public Order findOne(Long orderId) {
         return em.find(Order.class, orderId);
     }
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        return em.createQuery("select o from Order o", Order.class)
-//                .getResultList();
-//    }
+    // 동적 쿼리!
+    // 조건이 있다면 status, name으로 조건처리가 된 쿼리를 실행하고
+    // 조건이 없다면, 모든 데이터를 다 가져오도록 쿼리를 실행해야한다.
+    // JPQL: Order와 연관된 Member join!
+    public List<Order> findAll(OrderSearch orderSearch) {
+        /* 동적쿼리 없이 그냥 조건으로 검색하는 쿼리문.
+        em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status " +
+                        " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100)  // 페이징 : 100부터 시작해서
+                .setMaxResults(1000)    // 1000개로 최대 결과 제한
+                .getResultList();*/
+        /* 동적쿼리 생성 방법 3가지
+        1. JPQL : String으로 동적쿼리 생성하는 방법. 버그가 발생하기 쉬움.
+        2. JPA Criteria : JPA표준스펙. Java코드로 JPQL을 생성하도록 만들어주는 클래스 모음. 유지보수성이 현저히 떨어져 실무에서 사용X
+        3. QueryDSL : (권고) 동적 뿐만아니라 정적쿼리도 (복잡한 경우) 생성해줄 수 있음.
+         */
+
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status " +
+                        " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100)  // 페이징 : 100부터 시작해서
+                .setMaxResults(1000)    // 1000개로 최대 결과 제한
+                .getResultList();
+    }
 }
